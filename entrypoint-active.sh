e03a821aa6b835053d9cd80506c0ca39e678c5f4
elia <live09912802156@gmail.com>
2025-08-02 22:48:53 +0330

    fixing the bugs related to the zookeepers journalnodes and namenodes

--- entrypoint-active.sh
+++
@@ -1,9 +0,0 @@
-# entrypoint-active.sh
-#!/usr/bin/env bash
-set -e
-
-if [ ! -d /hadoop/dfs/name/current ]; then
-  hdfs namenode -format "$CLUSTER_NAME" -force
-fi
-
-exec hdfs namenode
--- entrypoint-standby.sh
+++
@@ -1,7 +0,0 @@
-# entrypoint-standby.sh
-#!/usr/bin/env bash
-set -e
-
-hdfs namenode -bootstrapStandby -force -nonInteractive
-
-exec hdfs namenode

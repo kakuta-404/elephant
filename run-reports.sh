0577f2e434fc58e7467c57ee551aec41da4a7af2
Hami <hamiebad79@gmail.com>
2025-08-05 18:07:49 +0330

    Dockerize loader & makes run-reports.sh runnable by git bash

--- run-reports.sh
+++ run-reports.sh
@@ -1,4 +1,4 @@
 mvn -f ./java-spark-app/pom.xml clean package
 echo "Building java spark app was successful. Trying to submit the generated jar file to spark."
-docker exec -it spark-master-1 /spark/bin/spark-submit --class app.Main --master spark://spark-master-1:7077,spark-master-2:7077 /opt/spark-app/target/java-spark-app-1.0-SNAPSHOT.jar
+MSYS_NO_PATHCONV=1 docker exec -it spark-master-1 /spark/bin/spark-submit --class app.Main --master spark://spark-master-1:7077,spark-master-2:7077 /opt/spark-app/target/java-spark-app-1.0-SNAPSHOT.jar
 sleep 10

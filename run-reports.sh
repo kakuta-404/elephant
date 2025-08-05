c0c4aca7fc047e7226f2aa578d045a97b489db91
Hami <hamiebad79@gmail.com>
2025-08-05 12:14:32 +0330

    Update Docker Compose and run-reports script for Spark cluster configuration

--- run-reports.sh
+++ run-reports.sh
@@ -1,5 +1,4 @@
 mvn -f ./java-spark-app/pom.xml clean package
-echo "Buidling java spark app was successfull. Trying to submint the generated jar file to spark."
-docker exec -it spark-master /spark/bin/spark-submit --class app.Main --master spark://spark-master:7077 /opt/spark-app/target/java-spark-app-1.0-SNAPSHOT.jar
+echo "Building java spark app was successful. Trying to submit the generated jar file to spark."
+docker exec -it spark-master-1 /spark/bin/spark-submit --class app.Main --master spark://spark-master-1:7077,spark-master-2:7077 /opt/spark-app/target/java-spark-app-1.0-SNAPSHOT.jar
 sleep 10
-
